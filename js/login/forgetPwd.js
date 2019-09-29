0492f27457abb253ace5e5da809b394eb6991b8a
Eternal0101 <1872642516@qq.com>
2019-09-29 18:27:05 +0800

    add

--- js/login/forgetPwd.js
+++ js/login/forgetPwd.js
@@ -2,6 +2,14 @@ $(function () {
     avalon.ready(function () {
         window.vmForgetPwd = avalon.define({
             $id: 'root',
+            postData:{
+                '_token_':'',
+                'username': '',
+                'code': '',
+                'password': '',
+                'password_confirm': '',
+                '_t': '', //PERSONAL（个人） MERCHANT（商家） LOGISTICS（物流公司） PROXY （区域代理）
+            },
             onLoad: function () {
                 console.log('忘记密码');
             }
